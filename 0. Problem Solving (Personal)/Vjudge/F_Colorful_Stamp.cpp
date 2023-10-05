f3ed6b00558849710fc303495428a058605effed
aushamim <au.shamim0@gmail.com>
2023-10-05 20:05:02 +0600

    XPSC - Add Practice W1D6

--- 0. Problem Solving (Personal)/Vjudge/F_Colorful_Stamp.cpp
+++ 0. Problem Solving (Personal)/Vjudge/F_Colorful_Stamp.cpp
@@ -11,11 +11,32 @@ int main()
         string s;
         cin >> s;
 
-        if (n == 1)
+        int i = 0, r = 0, b = 0, w = 0;
+        bool flag = false;
+        while (i < n)
         {
-            cout << "NO" << endl;
-            continue;
+            r += (s[i] == 'R'); // if(s[i]=='R'){e++}
+            b += (s[i] == 'B');
+            w += (s[i] == 'W');
+
+            if (w)
+            {
+                flag |= ((r == 0 && b == 0) || (r >= 1 && b >= 1));
+
+                if (!flag)
+                {
+                    break;
+                }
+                if (i < n - 1)
+                {
+                    flag = false;
+                }
+                r = b = w = 0;
+            }
+            i++;
         }
+        flag |= ((r == 0 && b == 0) || (r >= 1 && b >= 1));
+        cout << (flag ? "YES" : "NO") << endl;
     }
     return 0;
 }
