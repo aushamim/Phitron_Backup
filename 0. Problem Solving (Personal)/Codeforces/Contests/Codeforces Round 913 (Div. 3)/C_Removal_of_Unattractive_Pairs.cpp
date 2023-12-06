70e23f00b52c0fe80f7c70153edec276c07a935a
aushamim <au.shamim0@gmail.com>
2023-12-06 13:31:43 +0600

    CF Round 913

--- 0. Problem Solving (Personal)/Codeforces/Contests/Codeforces Round 913 (Div. 3)/C_Removal_of_Unattractive_Pairs.cpp
+++ 0. Problem Solving (Personal)/Codeforces/Contests/Codeforces Round 913 (Div. 3)/C_Removal_of_Unattractive_Pairs.cpp
@@ -11,59 +11,20 @@ int main()
         cin >> n >> s;
 
         map<char, int> mp;
+        int mx = INT_MIN;
         for (auto c : s)
         {
             mp[c]++;
+            mx = max(mx, mp[c]);
         }
 
-        vector<int> v;
-        int chk = -1;
-        bool same = true;
-        for (auto [x, y] : mp)
+        if (mx > n / 2)
         {
-            cout << x << " " << y << endl;
-            if (chk == -1)
-            {
-                chk = y;
-            }
-            else
-            {
-                if (y != chk)
-                {
-                    same = false;
-                }
-            }
-            v.push_back(y);
-        }
-
-        if (same)
-        {
-            cout << n % 2 << endl;
+            cout << mx - (n - mx) << endl;
         }
         else
         {
-            sort(v.begin(), v.end(), greater<int>());
-
-            for (int i = 0; i < v.size(); i++)
-            {
-                if (v[i] == 0)
-                {
-                    continue;
-                }
-                for (int j = i + 1; j < v.size(); j++)
-                {
-                    int mn = min(v[i], v[j]);
-                    v[i] -= mn;
-                    v[j] -= mn;
-                }
-            }
-
-            int count = 0;
-            for (auto val : v)
-            {
-                count += val;
-            }
-            cout << count << endl;
+            cout << (n % 2 == 0 ? 0 : 1) << endl;
         }
     }
     return 0;
